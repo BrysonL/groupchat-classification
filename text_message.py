345013bf9ff7ec4ec6f8685231b56d9f710ea63d
BrysonL <sbl4tf@virginia.edu>
2023-08-23 17:42:43 -0700

    starting feature extraction

--- text_message.py
+++ text_message.py
@@ -2,11 +2,16 @@ from datetime import datetime, timezone, timedelta
 from message import Message
 import torch
 from emoji import unicode_codes
+from textblob import TextBlob
 
 class TextMessage(Message):
     def __init__(self, sender_name, timestamp_ms, content):
         super().__init__(sender_name, timestamp_ms)
-        self.content = content
+        if len(content) > 1401:
+            self.content = content[:1401] # Truncate messages longer than 1401 characters - this only impacts 3 out of 56k messages in my dataset
+            # print(f"Truncated message from {self.sender} at {datetime.fromtimestamp(self.timestamp / 1000, tz=timezone.utc).astimezone(timezone(timedelta(hours=-5))) } with length {len(content)} and content: {content}")
+        else:
+            self.content = content
         self.message_type = "text"
 
     def __str__(self):
@@ -36,7 +41,7 @@ class TextMessage(Message):
         Features:
         - Feature 1: Message length
         - Feature 2: Number of words
-        - Feature 3: Presence of Emojis
+        - Feature 3: Sentiment Score (Polarity)
         - Feature 4: Presence of URLs
         - Feature 5: Presence of Numbers
         - Feature 6: Message Hour
@@ -44,7 +49,8 @@ class TextMessage(Message):
         - Feature 8: Presence of Question Marks
         - Feature 9: Month of the message
         - Feature 10: Number of lowercase letters
-        - Feature 11: Ratio of lowercase to uppercase letters
+        - Feature 11: Ratio of lowercase to all letters
+        - Feature 12: Average word length
         """
         # Feature 1: Extract message length
         message_length = len(self.content)
@@ -52,9 +58,9 @@ class TextMessage(Message):
         # Feature 2: Extract number of words
         num_words = len(self.content.split())
 
-        # Feature 3: Presence of Emojis
-        emojis = [char for char in self.content if char in unicode_codes.EMOJI_DATA]
-        has_emoji = 1 if emojis else 0
+        # Feature 3: Sentiment Score (Polarity)
+        blob = TextBlob(self.content)
+        sentiment_score = blob.sentiment.polarity
 
         # Feature 4: Presence of URLs
         has_url = 1 if "http://" in self.content or "https://" in self.content else 0
@@ -82,5 +88,10 @@ class TextMessage(Message):
         # Feature 11: Ratio of lowercase to all letters
         lowercase_ratio = num_lowercase / message_length
 
-        return torch.tensor([message_length, num_words, has_emoji, has_url, has_number, message_hour, day_of_week, has_question, message_month, num_lowercase, lowercase_ratio])
+        # Feature 12: Average word length
+        words = self.content.split()
+        avg_word_length = sum(len(word) for word in words) / num_words if num_words > 0 else 0
+        
+
+        return torch.tensor([message_length, num_words, sentiment_score, has_url, has_number, message_hour, day_of_week, has_question, message_month, num_lowercase, lowercase_ratio, avg_word_length])
     
